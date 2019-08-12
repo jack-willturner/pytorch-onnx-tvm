aac8e006be0c54c53b61a1439305313e364d6699
Jack Turner <jackwilliamturner@icloud.com>
2019-08-12 11:17:54 +0100

    VGG

---
+++ models/vgg.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(64,  128, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv6 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.conv8 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.conv9 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv10 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.conv11 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.conv12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.conv13 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.fc1 = nn.Linear(512*7*7, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 1000)
+
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.maxpool1(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.conv5(out)
+        out = self.maxpool2(out)
+        out = self.conv6(out)
+        out = self.conv7(out)
+        out = self.conv8(out)
+        out = self.conv9(out)
+        out = self.maxpool3(out)
+        out = self.conv10(out)
+        out = self.conv11(out)
+        out = self.conv12(out)
+        out = self.conv13(out)
+        out = self.maxpool4(out)
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
+
+def vgg16():
+    return VGG16()
--- torch_to_onnx.py
+++ torch_to_onnx.py
@@ -1,18 +1,79 @@
 import torch
-import onnx
-from funcs import *
+import torch.onnx
+import torch.nn as nn
 from models import *
+import pandas as pd
+from count_ops import *
+import argparse
 
-dummy_input = torch.randn(1,32,224,224)
+parser = argparse.ArgumentParser(description='AutoTVM from ONNX checkpoints')
+parser.add_argument('--model', default='resnet50', type=str)
+args = parser.parse_args()
 
-s_conv  = nn.Conv2d(32,64,3)
-g_conv  = nn.Conv2d(32,64,3,groups=4)
-b_conv  = nn.Conv2d(32,32,3)
-bg_conv = nn.Conv2d(32,32,3,groups=4)
+dummy_input = torch.randn(1,3,224,224)
+if args.model == 'resnet50':
+    net = resnet50()
+    base_file = 'resnet/resnet50'
+    save_folder = ['resnet']
+elif args.model == 'vgg16':
+    net = vgg16()
+    base_file = 'vgg/vgg16'
+    save_folder = 'vgg'
 
-convs = [(s_conv,'standard'), (g_conv,'group'), (b_conv,'bottle'), (bg_conv,'bottlegroup')]
+_ = net(dummy_input)
+torch.onnx.export(net, dummy_input, str(base_file + '.onnx'))
 
-for conv,name in convs:
-    model = conv
-    _ = model(dummy_input)
-    torch.onnx.export(model, dummy_input, 'blocks/'+name+'.t7')
+# store a list of parameter counts and spatial dimensions for each layer
+df = []
+
+print(net)
+
+i = 0
+### iterate over layers and export to onnx
+for block in net.children():
+    if isinstance(block, nn.Conv2d):
+        fname = base_file + '_' + str(i) + '.onnx'
+        ops, params = measure_layer(block, dummy_input)
+        df.append([fname,block.in_channels, block.out_channels, block.kernel_size,
+                        block.stride, block.padding, block.bias is not None, dummy_input.size()[2], dummy_input.size()[3], ops, params])
+        torch.onnx.export(block, dummy_input, fname)
+        i += 1
+        if args.model == 'vgg16':
+            dummy_input = block(dummy_input)
+
+    elif isinstance(block, nn.Sequential):
+        for layer in block:
+            fname = base_file + '_' + str(i) + '.onnx'
+            dummy_input = torch.randn(1,layer.conv1.in_channels,layer.conv1_input_size[2],layer.conv1_input_size[3])
+            ops, params = measure_layer(layer.conv1,dummy_input)
+            df.append([fname,layer.conv1.in_channels, layer.conv1.out_channels, layer.conv1.kernel_size,
+                            layer.conv1.stride, layer.conv1.padding, layer.conv1.bias, int(layer.conv1_input_size[2]),
+                            int(layer.conv1_input_size[3]), ops, params])
+            torch.onnx.export(layer.conv1, dummy_input,fname)
+            i += 1
+
+            fname = base_file + '_' + str(i) + '.onnx'
+            dummy_input = torch.randn(1,layer.conv2.in_channels,layer.conv2_input_size[2],layer.conv2_input_size[3])
+            ops, params = measure_layer(layer.conv2, dummy_input)
+            df.append([fname,layer.conv2.in_channels, layer.conv2.out_channels, layer.conv2.kernel_size,
+                            layer.conv2.stride, layer.conv2.padding, layer.conv2.bias, int(layer.conv2_input_size[2]),
+                            int(layer.conv2_input_size[3]), ops, params])
+            torch.onnx.export(layer.conv2, dummy_input, fname)
+            i += 1
+
+            fname = base_file + '_' + str(i) + '.onnx'
+            dummy_input =  torch.randn(1,layer.conv3.in_channels,layer.conv3_input_size[2],layer.conv3_input_size[3])
+            ops, params = measure_layer(layer.conv3, dummy_input)
+            df.append([fname,layer.conv3.in_channels, layer.conv3.out_channels, layer.conv3.kernel_size,
+                            layer.conv3.stride, layer.conv3.padding, layer.conv3.bias, int(layer.conv3_input_size[2]),
+                            int(layer.conv3_input_size[3]), ops, params])
+            torch.onnx.export(layer.conv3,dummy_input, fname)
+            i += 1
+
+    elif isinstance(block, nn.Linear):
+        fname = base_file + '_l_' + str(i) + '.onnx'
+        torch.onnx.export(block, torch.randn(1,block.in_features), (fname))
+        i += 1
+
+df = pd.DataFrame(df, columns=['filename','in_channels', 'out_channels', 'kernel_size', 'stride', 'padding', 'bias', 'input_spatial_x', 'input_spatial_y', 'ops', 'params'])
+df.to_csv(save_folder+'/layer_info.csv')
